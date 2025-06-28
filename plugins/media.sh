db2e66533f15bcb1041084b914d079d50d8e1587
Macintosh Portable <edwinolivares123@gmail.com>
2025-06-28 00:50:41 -0700

    delete media item

--- plugins/media.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-echo "SONG: '$INFO'" >> /tmp/sketchybar_debug.log
-STATE="$(echo "$INFO" | jq -r '.state')"
-if [ "$STATE" = "playing" ]; then
-  MEDIA="$(echo "$INFO" | jq -r '.title + " - " + .artist')"
-  sketchybar --set $NAME label="$MEDIA" drawing=on
-else
-  sketchybar --set $NAME drawing=off
-fi
